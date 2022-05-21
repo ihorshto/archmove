fc0d88e2d191944905344cb50ae00484c5fbe7cd
ihorshto <igorshtogrin2016@gmail.com>
2022-05-21 13:29:37 +0300

    added header, header__content, slider, advantages sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function(){
+$(function () {
+
+ $('.slider__inner').slick({
+  prevArrow: '<button type="button" class="slick-prev"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 8V6H4L8 2L7 0L0 7L7 14L8 12L4 8H16Z" fill="#FD665E"/></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 8V6H12L8 2L9 0L16 7L9 14L8 12L12 8H0Z" fill="#FD665E"/></svg></button>',
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: false,
+ });
 
 });
